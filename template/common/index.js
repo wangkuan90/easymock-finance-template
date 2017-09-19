8944e3ac6d38a2415469847c6f8d8f8576a98764
wangkuan <wangkuan@souche.com>
2017-09-19 16:22:42 +0800

    add

--- template/common/index.js
+++ template/common/index.js
@@ -1,4 +1,4 @@
-<% _.forEach(config.projects, function(project){ %>import * as <%- $$.convertUrl(project.name) %> from './<%- project.name %>';<% }) %>
+<% _.forEach(config.projects, function(project){ %>import <%- $$.convertUrl(project.name) %> from './<%- project.name %>';<% }) %>
 
 export {<% _.forEach(config.projects, function(project, i){ %>
 	<%- $$.convertUrl(project.name) %><% if(config.projects.length - 1 !== i) { %>,<% } %><% }) %>
--- template/common/util.js
+++ template/common/util.js
@@ -1,18 +1,32 @@
-import axios from 'axios';
+import Http from '@souche-f2e/http-request';
 
-const instance = axios.create();
+const ajax = (url, params = {}) => {
+    params.url = url;
+    params.type = params.type || 'get';
+    return Http.ajax(params).then(res => {
+        if (res.code === '10001') {
+            // TODO 退出登录
+        }
+        if (res.message === 'OK' || res.success) {
+            return Promise.resolve(res.data);
+        } else {
+            throw res;
+        }
+    }).catch(err => {
+        // TODO 数据异常
+        return Promise.reject(err);
+    });
+};
 
-function createAPI(baseURL) {
-	return function(conf) {
-		conf = conf || {};
-		return instance(Object.assign({}, {
-			url: conf.url,
-			baseURL: baseURL,
-			method: conf.method
-		}, conf.opts));
-	};
-}
-
-export {
-	createAPI
+export default {
+    modelName(baseURL) {
+        return (params) => {
+            // TODO 请求预处理
+            params.opts = params.opts || {};
+            return ajax(baseURL + params.url, {
+                data: params.opts,
+                type: params.method
+            });
+        };
+    }
 };
