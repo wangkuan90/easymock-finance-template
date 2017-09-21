eadb73536827dd63293fe0bebe707a4d705bb175
wangkuan <wangkuan@souche.com>
2017-09-21 11:19:17 +0800

    feat: change model

--- template/model/index.js
+++ template/model/index.js
@@ -12,28 +12,31 @@ import instance from './instance';
   <% if(key !== 'data'){ %>
 class <%- key %> {
     <% _.mapKeys(value, function(cellValue, cellKey){ %>
-      <% if(cellKey === '_id_'){ continue;} %>
-      <% if(cellValue.description){ %>
-       // <%- cellValue.description %> 
-       // type @<%- cellValue.type %> 
-      <% } %>
-      <%- cellKey %>;
+      <% if(cellKey !== '_id_'){ %>
+          <% if(cellValue.description){ %>
+           // <%- cellValue.description %> 
+           // type @<%- cellValue.type %> 
+          <% } %>
+          <%- cellKey %>;
+       <% } %>
     <% }) %>
     constructor(data) {
         <% _.mapKeys(value, function(cellValue, cellKey){ %>
-          <% if(cellKey === '_id_'){ continue;} %>
-          this.<%- $$.getMethodName(cellKey) %>(data.<%- cellKey %>);
+          <% if(cellKey !== '_id_'){ %>
+            this.<%- $$.getMethodName(cellKey) %>(data.<%- cellKey %>);
+          <% } %>
         <% }) %>
     }
     
     <% _.mapKeys(value, function(cellValue, cellKey){ %>
-          <% if(cellKey === '_id_'){ continue;} %>
-          <%- $$.getMethodName(cellKey) %>() (value) {
-            if(typeof value !== 'string') {
-                console.log('error');
-            }
-            this.<%- cellKey %> = value;
-         }
+          <% if(cellKey !== '_id_'){ %>
+              <%- $$.getMethodName(cellKey) %>() (value) {
+                if(typeof value !== 'string') {
+                    console.log('error');
+                }
+                this.<%- cellKey %> = value;
+             }
+          <% } %>
     <% }) %>
 }
   <% } %>
