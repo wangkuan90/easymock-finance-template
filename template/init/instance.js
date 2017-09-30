ad7ce853bffdf6e07404663e3e26ca0c3dd1cea4
wangkuan <wangkuan@souche.com>
2017-09-30 14:17:43 +0800

    feat: version to 2.0.0

--- template/init/instance.js
+++ template/init/instance.js
@@ -2,7 +2,7 @@ import Util from '<%- $$.relative("util") %>';
 import config from '<%- $$.relative("config") %>';
 
 const baseUrl = {
-	mock: 'https://mock.souche-inc.com/mock<%- data.project._id %><%- data.project.url %>',
+	mock: 'https://mock.souche-inc.com/mock/<%- data.project._id %><%- data.project.url %>',
 	development: '',
 	prepub: '',
 	production: ''
