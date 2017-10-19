dbc10ec54199b1562a42a151247b3e677095004f
wangkuan <wangkuan@souche.com>
2017-10-19 15:49:48 +0800

    add

--- template/model/index.js
+++ template/model/index.js
@@ -31,9 +31,8 @@ class <%- $$.filterMethodName(value.modelType) %>Array {
     }
 }<% } %><% if(key !== 'data'){ %>
 class <%- $$.filterMethodName(key) %> extends DataCheck.Response{<% _.mapKeys(value, function(cellValue, cellKey){ %><% if(cellKey !== '_id_'){ %>
-<% if(cellValue.description){ %>
-    // <%- $$.filterDescription(cellValue.description) %><% } %><% if(cellValue.type !== 'array' && cellValue.type){ %><% if(cellValue.type === 'string'){ %>
-    @DataCheck.isString<%- cellValue.required ? '(true)' : '()' %><% }else if(cellValue.type === 'boolean'){ %>@DataCheck.isBoolean<%- cellValue.required ? '(true)' : '()' %><% }else if(cellValue.type === 'integer'){ %>@DataCheck.isInteger<%- cellValue.required ? '(true)' : '()' %><% } %><% } %> 
+    <% if(cellValue.description){ %>// <%- $$.filterDescription(cellValue.description) %><% } %>
+    <% if(cellValue.type !== 'array' && cellValue.type){ %><% if(cellValue.type === 'string'){ %>@DataCheck.isString<%- cellValue.required ? '(true)' : '()' %><% }else if(cellValue.type === 'boolean'){ %>@DataCheck.isBoolean<%- cellValue.required ? '(true)' : '()' %><% }else if(cellValue.type === 'integer'){ %>@DataCheck.isInteger<%- cellValue.required ? '(true)' : '()' %><% } %><% } %> 
     <%- cellKey %>;<% } %><% }) %>
     
     constructor(data) {
@@ -41,8 +40,7 @@ class <%- $$.filterMethodName(key) %> extends DataCheck.Response{<% _.mapKeys(va
         this.<%- $$.getMethodName(cellKey) %>(data.<%- cellKey %>);<% } %><% }) %>
     }<% _.mapKeys(value, function(cellValue, cellKey){ %><% if(cellKey !== '_id_'){ %>
     <%- $$.getMethodName(cellKey) %>(value) {
-        <% if(cellValue.type === 'array'){ %>
-        this.<%- cellKey %> = value.map(item => new <%- $$.getMethodName2(cellValue.items.$ref) %>(item));<% }else if(cellValue.type){ %>this.<%- cellKey %> = value;<% }else{ %>this.<%- cellKey %> = new <%- $$.getMethodName2(cellValue.$ref) %>(value);<% } %>
+        <% if(cellValue.type === 'array'){ %>this.<%- cellKey %> = value.map(item => new <%- $$.getMethodName2(cellValue.items.$ref) %>(item));<% }else if(cellValue.type){ %>this.<%- cellKey %> = value;<% }else{ %>this.<%- cellKey %> = new <%- $$.getMethodName2(cellValue.$ref) %>(value);<% } %>
     }<% } %><% }) %>
 }<% } %><% }) %>
 
@@ -53,7 +51,6 @@ export default (opts) => {<% if(data.parameters.length > 0){ %>
         url: '<%- data.url %>',
         opts: opts
     }).then(data => {
-        <% _.mapKeys(data.response_model, function(value, key){ %><% if(key === 'data'){ %><% if(value.type === 'array'){ %>
-        return new <%- $$.filterMethodName(value.modelType) %>Array(data);<% }else if($$.isNormalType(value.type)){ %>return data;<% }else{ %>return new <%- $$.filterMethodName(value.modelType) %>(data);<% } %><% } %><% }) %>
+        <% _.mapKeys(data.response_model, function(value, key){ %><% if(key === 'data'){ %><% if(value.type === 'array'){ %>return new <%- $$.filterMethodName(value.modelType) %>Array(data);<% }else if($$.isNormalType(value.type)){ %>return data;<% }else{ %>return new <%- $$.filterMethodName(value.modelType) %>(data);<% } %><% } %><% }) %>
     });
 };<% } %>
